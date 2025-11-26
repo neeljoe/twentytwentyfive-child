0920b10a3c69ad4fb131173b801fc641b8e53578
neeljoe <joe.neel@gmail.com>
2025-11-26 17:35:11 +0530

    test again

--- functions.php
+++ functions.php
@@ -2,36 +2,35 @@
 
 function tt5_child_enqueue_assets() {
 
-    // Correct path for Vite 5
     $manifest_path = get_stylesheet_directory() . '/build/.vite/manifest.json';
 
     if (!file_exists($manifest_path)) {
-        error_log('Manifest not found: ' . $manifest_path);
+        error_log("Manifest not found at: $manifest_path");
         return;
     }
 
     $manifest = json_decode(file_get_contents($manifest_path), true);
 
-    // Get JS file
-    $js_file = $manifest['src/main.js']['file'];
+    // JS
+    if (isset($manifest['src/main.js']['file'])) {
+        $js_file = $manifest['src/main.js']['file'];
+        wp_enqueue_script(
+            'tt5-child-main-js',
+            get_stylesheet_directory_uri() . '/build/' . $js_file,
+            [],
+            filemtime(get_stylesheet_directory() . '/build/' . $js_file),  // ⬅ Versioning
+            true
+        );
+    }
 
-    wp_enqueue_script(
-        'tt5-child-main-js',
-        get_stylesheet_directory_uri() . '/build/' . $js_file,
-        array(),
-        null,
-        true
-    );
-
-    // Get CSS file (if exists)
+    // CSS
     if (isset($manifest['src/main.js']['css'][0])) {
         $css_file = $manifest['src/main.js']['css'][0];
-
         wp_enqueue_style(
             'tt5-child-main-css',
             get_stylesheet_directory_uri() . '/build/' . $css_file,
-            array(),
-            null
+            [],
+            filemtime(get_stylesheet_directory() . '/build/' . $css_file)  // ⬅ Versioning
         );
     }
 }
